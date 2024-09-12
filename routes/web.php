e83cd693a9783d87120f644e64b550faa69bbaf0
MuhammadTaufiqAbdusSalam <arekmoker65@gmail.com>
2024-09-12 18:51:19 +0700

    upload praktikum 5

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 route::get('/level', [LevelController::class, 'index']);
+route::get('/kategori', [KategoriController::class, 'index']);
