a853024b2e692341ac62875779b7e994e1795496
MuhammadTaufiqAbdusSalam <arekmoker65@gmail.com>
2024-09-19 23:04:28 +0700

    upload js 4 prak 2.7

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,57 +11,57 @@ use Illuminate\Http\Request;
 use App\Models\UserModel;
 class UserController extends Controller
 {
-    public function index()
-    {
-        //$row = DB::update('update m_user set user_id = ?, level_id = ? where username = ?', [4, 4, 'customer-1']);
-        //return 'Update data berhasil. Jumlah data yang diupdate: ' . $row . ' baris';
+    // public function index()
+    // {
+    //     //$row = DB::update('update m_user set user_id = ?, level_id = ? where username = ?', [4, 4, 'customer-1']);
+    //     //return 'Update data berhasil. Jumlah data yang diupdate: ' . $row . ' baris';
 
-        // $data =[
-        //     //'username'=>'customer-1',
-        //     //'nama'=>'Pelanggan pertama',
-        //     //'password'=> Hash::make('12345'),
-        //     //'level_id'=> 4
-        //     'level_id' => 2,
-        //     'username' => 'Manager_tiga',
-        //     'nama' => 'Manager 3',A
-        //     'password' => Hash::make('12345'),
-        // ];
-        // UserModel::create($data);
+    //     // $data =[
+    //     //     //'username'=>'customer-1',
+    //     //     //'nama'=>'Pelanggan pertama',
+    //     //     //'password'=> Hash::make('12345'),
+    //     //     //'level_id'=> 4
+    //     //     'level_id' => 2,
+    //     //     'username' => 'Manager_tiga',
+    //     //     'nama' => 'Manager 3',A
+    //     //     'password' => Hash::make('12345'),
+    //     // ];
+    //     // UserModel::create($data);
 
-    //    UserModel::where('username','customer-1')->update($data);
-    // Mencoba akses model UserModel
-    // $user = UserModel::create(
-    //     [
-    //         'username' => 'manager11',
-    //         'nama' => 'Manager11',
-    //         'password' => Hash::make('12345'),
-    //         'level_id' => 2
-    //     ]
-    // );
+    // //    UserModel::where('username','customer-1')->update($data);
+    // // Mencoba akses model UserModel
+    // // $user = UserModel::create(
+    // //     [
+    // //         'username' => 'manager11',
+    // //         'nama' => 'Manager11',
+    // //         'password' => Hash::make('12345'),
+    // //         'level_id' => 2
+    // //     ]
+    // // );
 
-    // $user->username = 'manager12';
-    // $user->isDirty();
-    // $user->isDirty('username');
-    // $user->isDirty('nama');
-    // $user->isDirty(['nama', 'username']);
+    // // $user->username = 'manager12';
+    // // $user->isDirty();
+    // // $user->isDirty('username');
+    // // $user->isDirty('nama');
+    // // $user->isDirty(['nama', 'username']);
 
-    // $user->isClean();
-    // $user->isClean('username');
-    // $user->isClean('nama');
-    // $user->isClean(['nama', 'username']);
+    // // $user->isClean();
+    // // $user->isClean('username');
+    // // $user->isClean('nama');
+    // // $user->isClean(['nama', 'username']);
 
-    // $user->save();
-    // $user->wasChanged();
-    // $user->wasChanged('username');
-    // $user->wasChanged(['username', 'level_id']);
-    // $user->wasChanged('nama');
-    // dd($user->wasChanged(['nama', 'username']));
-    // $user->isDirty();
-    // $user->isClean();
-    // dd($user-> isDirty());
-    $user = UserModel::all();
-    return view('user', ['data' => $user]);
-    }
+    // // $user->save();
+    // // $user->wasChanged();
+    // // $user->wasChanged('username');
+    // // $user->wasChanged(['username', 'level_id']);
+    // // $user->wasChanged('nama');
+    // // dd($user->wasChanged(['nama', 'username']));
+    // // $user->isDirty();
+    // // $user->isClean();
+    // // dd($user-> isDirty());
+    // $user = UserModel::all();
+    // return view('user', ['data' => $user]);
+    // }
     public function tambah()
     {
         return view('User_tambah');
@@ -95,5 +95,10 @@ class UserController extends Controller
         $user = UserModel::find($id);
         $user->delete();
         return redirect('/user');
+    }
+    public function index()
+    {
+        $user = UserModel::with('level')->get();
+        return view('user', ['data' => $user]);
     }
 }
