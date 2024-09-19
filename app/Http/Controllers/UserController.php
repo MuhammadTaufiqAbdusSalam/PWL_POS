47bc2916eb6908c1f3bb23fa41b8a2a6774c1cd5
MuhammadTaufiqAbdusSalam <arekmoker65@gmail.com>
2024-09-19 20:55:24 +0700

    upload prak 2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,7 +29,15 @@ class UserController extends Controller
 
     //    UserModel::where('username','customer-1')->update($data);
     // Mencoba akses model UserModel
-    $user = UserModel::where('level_id', 2)->count(); //Mengambil semua data dari tabel m_user
+    $user = UserModel::firstOrNew(
+        [
+            'username' => 'manager33',
+            'nama' => 'Manager Tiga Tiga',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
+        ]
+    );
+    $user->save();
     return view('user', ['data' => $user]);
     }
 }
