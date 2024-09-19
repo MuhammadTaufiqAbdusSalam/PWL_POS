b5f48ee405ca837739f70b8473f5ff2e2939a96a
MuhammadTaufiqAbdusSalam <arekmoker65@gmail.com>
2024-09-19 21:16:25 +0700

    upload prak 2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,15 +29,35 @@ class UserController extends Controller
 
     //    UserModel::where('username','customer-1')->update($data);
     // Mencoba akses model UserModel
-    $user = UserModel::firstOrNew(
+    $user = UserModel::create(
         [
-            'username' => 'manager33',
-            'nama' => 'Manager Tiga Tiga',
+            'username' => 'manager11',
+            'nama' => 'Manager11',
             'password' => Hash::make('12345'),
             'level_id' => 2
         ]
     );
+
+    $user->username = 'manager12';
+    // $user->isDirty();
+    // $user->isDirty('username');
+    // $user->isDirty('nama');
+    // $user->isDirty(['nama', 'username']);
+
+    // $user->isClean();
+    // $user->isClean('username');
+    // $user->isClean('nama');
+    // $user->isClean(['nama', 'username']);
+
     $user->save();
-    return view('user', ['data' => $user]);
+    $user->wasChanged();
+    $user->wasChanged('username');
+    $user->wasChanged(['username', 'level_id']);
+    $user->wasChanged('nama');
+    dd($user->wasChanged(['nama', 'username']));
+    // $user->isDirty();
+    // $user->isClean();
+    // dd($user-> isDirty());
+    //return view('user', ['data' => $user]);
     }
 }
